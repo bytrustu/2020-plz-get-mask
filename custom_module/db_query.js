7bc0f1e79db368946978b09bcb9bcb54b23751f8
bytrustu <bytrustu@gmail.com>
2020-02-26 23:43:26 +0900

    change db setting

--- custom_module/db_query.js
+++ custom_module/db_query.js
@@ -1,183 +1,250 @@
-const getDB = require('./mysql_connect.js').getDB;
 const async = require('async');
+const db_pool = require('./mysql_connect.js').getPool();
 
-module.exports.getServerStatus = (cb__) => {
-    getDB({
-        callback: cb__,
-        fail_val: false,
-        is_transaction: false
-    }).then(([db, callback]) => {
-        const query = `select * from maskinfo;`;
-        const query_list = [];
-        db.query(query, query_list, function (err, data) {
+module.exports.getServerStatus = (callback) => {
+    db_pool.getConnection(function(err, db) {
+        try {
             if (err) {
                 callback(false);
             } else {
-                callback(data[0]);
-            }
-        });
-    });
-};
-
-module.exports.getNonCallUserList = (cb__) => {
-    getDB({
-        callback: cb__,
-        fail_val: false,
-        is_transaction: false
-    }).then(([db, callback]) => {
-        const query = `select id from User where is_call_user != 1;`;
-        const query_list = [];
-        db.query(query, query_list, function (err, data) {
-            if (err) {
-                callback(false);
-            } else {
-                callback(data);
-            }
-        });
-    });
-};
-
-module.exports.updateMaskList = (info, cb__) => {
-    getDB({
-        callback: cb__,
-        fail_val: false,
-        is_transaction: true
-    }).then(([db, callback]) => {
-        async.series([
-                function (cb) {
-                    const query = "delete from masklist";
-                    const query_list = [];
-                    db.query(query, query_list, function (err) {
-                        if (err) {
-                            db.rollback();
-                            callback(false);
-                        } else {
-                            cb(null, null);
-                        }
-                    });
-                },
-                function (cb) {
-                    async.eachSeries(info, function (item, cb2) {
-                            const {name, url, etc} = item;
-                            const query = "insert into masklist values(?, ?, ?);";
-                            const query_list = [name, url, etc];
-                            db.query(query, query_list, (err, result) => {
-                                if (err) {
-                                    db.rollback();
-                                    callback(false);
-                                } else {
-                                    cb2();
-                                }
-                            });
-                        },
-                        function (err) {
-                            cb(null, null);
+                db.beginTransaction(function (err) {
+                    if (err) {
+                        db.rollback();
+                        callback(false);
+                    } else {
+                        const query = `select * from maskinfo;`;
+                        const query_list = [];
+                        db.query(query, query_list, function (err, data) {
+                            if (err) {
+                                callback(false);
+                            } else {
+                                callback(data[0]);
+                            }
                         });
-                },
-                function (cb) {
-                    db.commit(function (err) {
-                        if (err) {
-                            db.rollback();
-                            callback(false);
-                        } else {
-                            cb(null, null);
-                        }
-                    });
-                }
-            ],
-            function (err, results) {
-                callback(true);
-            });
+                    }
+                });
+            }
+        } catch(e){}finally{
+            db.release();
+        }
     });
 };
 
-
-module.exports.initMaskUrl = (info, cb__) => {
-    getDB({
-        callback: cb__,
-        fail_val: false,
-        is_transaction: true
-    }).then(([db, callback]) => {
-        async.series([
-                function (cb) {
-                    const query = "delete from maskurl";
-                    const query_list = [];
-                    db.query(query, query_list, function (err) {
-                        if (err) {
-                            db.rollback();
-                            callback(false);
-                        } else {
-                            cb(null, null);
-                        }
-                    });
-                },
-                function (cb) {
-                    async.eachSeries(info, function (item, cb2) {
-                            const {name, url, etc} = item;
-                            const query = "insert into maskurl values(?, ?, ?);";
-                            const query_list = [name, url, etc];
-                            db.query(query, query_list, (err, result) => {
-                                if (err) {
-                                    db.rollback();
-                                    callback(false);
-                                } else {
-                                    cb2();
-                                }
-                            });
-                        },
-                        function (err) {
-                            cb(null, null);
+module.exports.getNonCallUserList = (callback) => {
+    db_pool.getConnection(function(err, db) {
+        try {
+            if (err) {
+                callback(false);
+            } else {
+                db.beginTransaction(function (err) {
+                    if (err) {
+                        db.rollback();
+                        callback(false);
+                    } else {
+                        const query = `select id from User where is_call_user != 1;`;
+                        const query_list = [];
+                        db.query(query, query_list, function (err, data) {
+                            if (err) {
+                                callback(false);
+                            } else {
+                                callback(data);
+                            }
                         });
-                },
-                function (cb) {
-                    db.commit(function (err) {
-                        if (err) {
-                            db.rollback();
-                            callback(false);
-                        } else {
-                            cb(null, null);
-                        }
-                    });
-                }
-            ],
-            function (err, results) {
-                callback(true);
-            });
+                    }
+                });
+            }
+        } catch(e){}finally{
+            db.release();
+        }
     });
 };
 
-module.exports.getMaskUrl = (cb__) => {
-    getDB({
-        callback: cb__,
-        fail_val: false,
-        is_transaction: false
-    }).then(([db, callback]) => {
-        const query = `select * from maskurl;`;
-        const query_list = [];
-        db.query(query, query_list, function (err, data) {
+module.exports.updateMaskList = (info, callback) => {
+    db_pool.getConnection(function(err, db) {
+        try {
             if (err) {
                 callback(false);
             } else {
-                callback(data);
+                db.beginTransaction(function (err) {
+                    if (err) {
+                        db.rollback();
+                        callback(false);
+                    } else {
+                        async.series([
+                                function (cb) {
+                                    const query = "delete from masklist";
+                                    const query_list = [];
+                                    db.query(query, query_list, function (err) {
+                                        if (err) {
+                                            db.rollback();
+                                            callback(false);
+                                        } else {
+                                            cb(null, null);
+                                        }
+                                    });
+                                },
+                                function (cb) {
+                                    async.eachSeries(info, function (item, cb2) {
+                                            const {name, url, etc} = item;
+                                            const query = "insert into masklist values(?, ?, ?);";
+                                            const query_list = [name, url, etc];
+                                            db.query(query, query_list, (err, result) => {
+                                                if (err) {
+                                                    db.rollback();
+                                                    callback(false);
+                                                } else {
+                                                    cb2();
+                                                }
+                                            });
+                                        },
+                                        function (err) {
+                                            cb(null, null);
+                                        });
+                                },
+                                function (cb) {
+                                    db.commit(function (err) {
+                                        if (err) {
+                                            db.rollback();
+                                            callback(false);
+                                        } else {
+                                            cb(null, null);
+                                        }
+                                    });
+                                }
+                            ],
+                            function (err, results) {
+                                callback(true);
+                            });
+                    }
+                });
             }
-        });
+        } catch(e){}finally{
+            db.release();
+        }
     });
 };
 
-module.exports.getMaskList = (cb__) => {
-    getDB({
-        callback: cb__,
-        fail_val: false,
-        is_transaction: false
-    }).then(([db, callback]) => {
-        const query = `select * from masklist;`;
-        const query_list = [];
-        db.query(query, query_list, function (err, data) {
+
+module.exports.initMaskUrl = (info, callback) => {
+    db_pool.getConnection(function(err, db) {
+        try {
             if (err) {
                 callback(false);
             } else {
-                callback(data);
+                db.beginTransaction(function (err) {
+                    if (err) {
+                        db.rollback();
+                        callback(false);
+                    } else {
+                        async.series([
+                                function (cb) {
+                                    const query = "delete from maskurl";
+                                    const query_list = [];
+                                    db.query(query, query_list, function (err) {
+                                        if (err) {
+                                            db.rollback();
+                                            callback(false);
+                                        } else {
+                                            cb(null, null);
+                                        }
+                                    });
+                                },
+                                function (cb) {
+                                    async.eachSeries(info, function (item, cb2) {
+                                            const {name, url, etc} = item;
+                                            const query = "insert into maskurl values(?, ?, ?);";
+                                            const query_list = [name, url, etc];
+                                            db.query(query, query_list, (err, result) => {
+                                                if (err) {
+                                                    db.rollback();
+                                                    callback(false);
+                                                } else {
+                                                    cb2();
+                                                }
+                                            });
+                                        },
+                                        function (err) {
+                                            cb(null, null);
+                                        });
+                                },
+                                function (cb) {
+                                    db.commit(function (err) {
+                                        if (err) {
+                                            db.rollback();
+                                            callback(false);
+                                        } else {
+                                            cb(null, null);
+                                        }
+                                    });
+                                }
+                            ],
+                            function (err, results) {
+                                callback(true);
+                            });
+                    }
+                });
             }
-        });
+        } catch(e){}finally{
+            db.release();
+        }
+    });
+};
+
+module.exports.getMaskUrl = (callback) => {
+    db_pool.getConnection(function(err, db) {
+        try {
+            if (err) {
+                callback(false);
+            } else {
+                db.beginTransaction(function (err) {
+                    if (err) {
+                        db.rollback();
+                        callback(false);
+                    } else {
+                        const query = `select * from maskurl;`;
+                        const query_list = [];
+                        db.query(query, query_list, function (err, data) {
+                            if (err) {
+                                callback(false);
+                            } else {
+                                callback(data);
+                            }
+                        });
+
+                    }
+                });
+            }
+        } catch(e){}finally{
+            db.release();
+        }
+    });
+};
+
+module.exports.getMaskList = (callback) => {
+    db_pool.getConnection(function(err, db) {
+        try {
+            if (err) {
+                callback(false);
+            } else {
+                db.beginTransaction(function (err) {
+                    if (err) {
+                        db.rollback();
+                        callback(false);
+                    } else {
+                        const query = `select * from masklist;`;
+                        const query_list = [];
+                        db.query(query, query_list, function (err, data) {
+                            if (err) {
+                                callback(false);
+                            } else {
+                                callback(data);
+                            }
+                        });
+                    }
+                });
+            }
+        } catch(e){}finally{
+            db.release();
+        }
     });
 };
--- custom_module/mysql_connect.js
+++ custom_module/mysql_connect.js
@@ -1,7 +1,7 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
 module.exports.mysqlconfig = {
-    host : "localhost",
+    host : "127.0.0.1",
     port : "3306",
     user : "root",
     password : "root",
@@ -10,37 +10,6 @@ module.exports.mysqlconfig = {
     waitForConnections:true
 };
 
-const db_pool = mysql.createPool(module.exports.mysqlconfig);
-module.exports.getDB=function(options){
-    let {callback, fail_val, is_transaction} = options;
-    if (!callback || typeof(callback) != 'function') callback = function() {};
-    return new Promise(function (resolve, reject) {
-        db_pool.getConnection(function(err, db){
-            if (err) {
-                callback(fail_val);
-                reject();
-            } else {
-                if (is_transaction) {
-                    db.beginTransaction(function(err){
-                        if (err){
-                            db.rollback();
-                            callback(fail_val);
-                            reject();
-                        } else {
-                            resolve([db, function() {
-                                db.rollback(); // 자동 롤백
-                                db.release(); // 자동 release
-                                if (typeof(callback) == "function") callback.apply(null, arguments);
-                            }]);
-                        }
-                    });
-                } else {
-                    resolve([db, function() {
-                        db.release(); // 자동 release
-                        if (typeof(callback) == "function") callback.apply(null, arguments);
-                    }]);
-                }
-            }
-        });
-    });
-}
+module.exports.getPool=function(){
+    return mysql.createPool(module.exports.mysqlconfig);
+};
